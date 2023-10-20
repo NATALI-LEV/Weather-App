3a8e6a6c669e967635bf76b7ffd1e74ca6af50ab
NATALI-LEV <natalillev@gmail.com>
2023-10-20 15:44:14 +0300

    finished current

--- app/component/Current.tsx
+++ app/component/Current.tsx
@@ -1,6 +1,23 @@
 import { getCurrentDate } from "../utils/currentDate";
+import { MdLocationOn } from "react-icons/md";
 
-const Current = ({data}) => {
+interface CurrentProps {
+    data: {
+        current: {
+            condition: {
+                icon: string;
+                text: string;
+            };
+            temp_f:number;
+        };
+        location: {
+            name:string;
+            region:string;
+        };
+    };
+}
+
+const Current = ({data}: CurrentProps) => {
     const currentDate = getCurrentDate();
     const weatherIcon = data.current.condition.icon;
     return (
@@ -16,11 +33,21 @@ const Current = ({data}) => {
                 </div>
             )}
         </div>
+        <div>
         <p className="text-5xl text-white">
             {data.current.temp_f.toFixed()}
             <span>°</span>
         </p>
         <span className="text-white">{data.current.condition.text}</span>
+        </div>
+        <div>
+            <div className="flex items-center text-black bg-white/90 px-2 py-2 rounded-xl">
+                <MdLocationOn />
+                <span>
+                    {data.location.name}, {data.location.region}
+                </span>
+            </div>
+        </div>
     </div>
   );
 };
